eddf6d92d3191afd87120542eaf68ede50fe7e89
sergioSEa <sergioandreusanchez@gmail.com>
2022-01-18 17:45:00 +0100

    Included: Pathway analysis

--- GUTS_script.R
+++ GUTS_script.R
@@ -7,10 +7,27 @@ library(ggrepel)
 ggplot <- function(...) ggplot2::ggplot(...) + scale_color_manual(values=met.brewer("Manet",5)[c(1,5)] ) +
   scale_fill_manual(values=met.brewer("Manet", 5)[c(1,5)] )
 
-
+Clean_path = function(C, Keep){
+  colnames(C)[1] = "ID"
+  sapply(colnames(C), function(x){ str_split(x, "_knead")[[1]][1] } ) -> New_names
+  colnames(C) = New_names
+  C %>% select(c("ID", Keep$ID)) -> C
+  return(C)
+}
+Format_path = function(Path){
+  Path %>% filter(! grepl("#", ID)) -> Path
+  Path %>% filter(! grepl("\\|",ID )) -> Path
+  t(Path) %>% as.data.frame() %>% rownames_to_column("ID") %>% as_tibble() %>% filter(! ID == "ID") -> Path_2
+  colnames(Path_2) = c("ID",  Path$ID)
+  return(Path_2)
+}
 
 Case = read_tsv("Cases.tsv")
 Control = read_tsv("Matched_controls.tsv")
+
+#Pathway abundances
+Case_ptw = Format_path(Clean_path(read_tsv("merged_Pathway_table_cases.txt"), Keep = Case ))
+Control_ptw = Format_path(Clean_path(read_tsv("merged_Pathway_table_controls.txt"), Keep = Control)) 
 
 #Covariate Read number
 Case_readN = read_tsv("Read_number.tsv") %>%  mutate(Cohort = "Case") 
@@ -20,8 +37,8 @@ Read_number %>% ggplot(aes(x=Read_number, fill=Cohort )) + geom_histogram(alpha=
 #1. Compute prevalences
 Comp_Prevalence = function(DF){
   #Compute prevalence of different taxa
-  apply(DF,2, function(x){ sum(!x==0)/length(x) } ) -> Prevalence_vector
-  apply(DF,2, function(x){ sum(!x==0) } ) -> N
+  apply(DF,2, function(x){ sum(!as.numeric(x)==0)/length(x) } ) -> Prevalence_vector
+  apply(DF,2, function(x){ sum(!as.numeric(x)==0) } ) -> N
   Prevalence_df = tibble(Bug = names(Prevalence_vector), Prevalence = Prevalence_vector, N = N)
   return(Prevalence_df)
 }
@@ -49,12 +66,13 @@ CLR = function(D){
   log(D / Geom_mean(D) )
   
 }
-Compute_CLR_taxonomy = function(Data, Taxonomy = "s"){
-  paste( c(Taxonomy, "__"), collapse="" ) -> Ta
-  colnames(select(Data, -c(ID, UNKNOWN ))) -> Taxa
-  Taxa[grepl(Ta, Taxa)] -> Taxa
-  Data %>% select( c("ID","UNKNOWN", Taxa) ) -> Data_taxonomy
-  
+Compute_CLR_taxonomy = function(Data, Taxonomy = "s", Keep_column = "UNKNOWN"){
+  if ("Taxonomy" == "Path"){
+    paste( c(Taxonomy, "__"), collapse="" ) -> Ta
+    colnames(select(Data, -c("ID", Keep_column ))) -> Taxa
+    Taxa[grepl(Ta, Taxa)] -> Taxa
+    Data %>% select( c("ID", Keep_column, Taxa) ) -> Data_taxonomy
+  } else { Data_taxonomy = Data}
   #computation of a different pseudocoount per taxa.
   apply(select(Data_taxonomy, -ID), 2, function(taxa){
     if( sum(taxa==0) >= 1){ pscount =  Pseudocount(taxa) 
@@ -64,7 +82,7 @@ Compute_CLR_taxonomy = function(Data, Taxonomy = "s"){
     P = CLR(Participant + Pseudocounts)
     P
   }) %>% t() %>% as_tibble() %>% mutate(ID = Data_taxonomy$ID, .before=1) -> Data_taxonomy
-  Data_taxonomy %>% select(-UNKNOWN) -> Data_taxonomy
+  Data_taxonomy %>% select(-Keep_column) -> Data_taxonomy
   return(Data_taxonomy)
 }  
 
@@ -79,6 +97,7 @@ for (i in c("p", "c", "o", "f", "g")){
 #Compute beta diversity at the species level
 Beta_taxonomy = function(Data_t, Data_c, Meta = Read_number){
   #Compute diversity
+  #Euclidean distance in CLR-transformed data corresponds to Aitchison distance.
   vegdist(select(Data_t, -ID), method = "euclidean") -> Beta_diversity
   
   #A PCOA
@@ -125,12 +144,13 @@ Alpha_taxonomy(Data, Data_c, "s")
 library(codacore)
 library(tensorflow)
 library(pROC)
-Run_balance_analysis = function(Balance_input, Labels = Data_c$Cohort, lambda=1){
+Run_balance_analysis = function(Balance_input, Labels = Data_c$Cohort, lambda=1, add_pseudo = T){
   set.seed(50)
-  Balance_input %>% select(-ID) -> Balance_input2 
-  Pseudo = apply(Balance_input2, 2, Pseudocount)
-  Balance_input2 = as.data.frame(Balance_input2) + Pseudo
-  
+  Balance_input %>% select(-ID) -> Balance_input2
+  if (add_pseudo == T){
+    Pseudo = apply(Balance_input2, 2, Pseudocount)
+    Balance_input2 = as.data.frame(Balance_input2) + Pseudo
+  }
   #Split dataset
   tf$random$set_seed(0)
   trainIndex <- sample(1:nrow( Balance_input2), 0.8 * nrow(Balance_input2))
@@ -146,9 +166,10 @@ Run_balance_analysis = function(Balance_input, Labels = Data_c$Cohort, lambda=1)
   print("Train results")
   codacoreAUC = model$ensemble[[1]]$AUC
   cat("Train set AUC (model 1) =", codacoreAUC, "\n")
-  codacoreAUC = model$ensemble[[2]]$AUC
-  cat("Train set AUC (model 2) =", codacoreAUC, "\n")
-  
+  if (length(model$ensemble) > 1){ 
+    codacoreAUC = model$ensemble[[2]]$AUC
+    cat("Train set AUC (model 2) =", codacoreAUC, "\n")
+  }
   ##Test
   yHat <- predict(model, newx = xTest)
   print("Test results")
@@ -164,10 +185,14 @@ Run_balance_analysis = function(Balance_input, Labels = Data_c$Cohort, lambda=1)
   plotROC(model)
   Numerators = colnames(xTrain)[getNumeratorParts(model, 1)] 
   Denominators = colnames(xTrain)[getDenominatorParts(model, 1)]
-  Numerators2 = colnames(xTrain)[getNumeratorParts(model, 2)] 
-  Denominators2 = colnames(xTrain)[getDenominatorParts(model, 2)]
+ 
   
-  return(list(Numerators, Denominators, Numerators2, Denominators2))
+  if (length(model$ensemble) > 1){ 
+    Numerators2 = colnames(xTrain)[getNumeratorParts(model, 2)] 
+    Denominators2 = colnames(xTrain)[getDenominatorParts(model, 2)]
+    return(list(Numerators, Denominators, Numerators2, Denominators2))
+    
+  }else { return(list(Numerators, Denominators)) }
 }   
 
 #Get only prevalent bacteria for the model
@@ -200,6 +225,7 @@ Association_analysis = function(Prevalence, DF, Data_c, FILTER=20, Meta=Read_num
     #invers rank normal transf?
     Data_c %>% select(ID, Cohort) %>% mutate(B =  vector_Bug, B_n = normalized_Bug) -> Model_input
     left_join(Model_input, Meta) -> Model_input
+    Model_input %>% mutate(Cohort = factor(Cohort,  levels = c("Control", "Case") )) -> Model_input
     lm(B ~ Cohort+ Read_number, Model_input  ) -> Model_out
     lm(B_n ~ Cohort+ Read_number, Model_input  ) -> Model_out_n
     #If not normalized
@@ -258,5 +284,61 @@ Top_results %>% ggplot(aes(x=Estimate_norm, y= -log10(`Pr(>|t|)_norm`), col=FDR_
   geom_label_repel(data = Top_results %>% filter(FDR_permutation<0.05) %>% unique(), 
                    aes(label = Bug),
                    color = "black")
-write_tsv(Top_results, "Sumamry_statistics_TaxonomyAnalysis.csv")  
+write_tsv(Top_results, "Summary_statistics_TaxonomyAnalysis.csv")  
 
+
+######################
+## Pathway analysis###
+######################
+
+#Get prevalences
+Comp_Prevalence( select(Case_ptw, -ID) ) -> Prevalence_cases_ptw
+Comp_Prevalence( select(Control_ptw, -ID) ) -> Prevalence_controls_ptw
+full_join(Prevalence_cases_ptw, Prevalence_controls_ptw, by="Bug", suffix = c("_case", "_control") ) %>% drop_na() -> Prevalence_ptw
+
+#Prepare data
+rbind( select(Control_ptw, c(ID, Prevalence_ptw$Bug) ), select(Case_ptw, c(ID, Prevalence_ptw$Bug) ))  -> Data_ptw
+Data_ptw %>% select(-ID) %>% apply(2, as.numeric) %>% as.data.frame() %>% mutate(ID = Data_ptw$ID) -> Data_ptw
+
+Data_ptw %>% mutate(Cohort = ifelse(grepl("LL", ID), "Control", "Case" )) -> Data_c_ptw
+Data_c_ptw$Cohort = factor(Data_c_ptw$Cohort, levels= c("Control", "Case"))
+
+
+#Prediction## Using only fully prevalent taxa
+Prevalence_ptw %>% filter(Prevalence_case == 1 & Prevalence_control == 1) %>% filter(! Bug %in% c("UNMAPPED", "UNINTEGRATED")) -> Keep_ptw
+Run_balance_analysis( Balance_input = select(Data_ptw, c("ID", Keep_ptw$Bug)) , Labels = Data_c_ptw$Cohort, lambda= 1, add_pseudo = F  ) -> Balances_ptw
+Data_ptw %>% select(Balances_ptw[[1]]) %>% apply(1, sum) -> Numerator_ptw ; Data_ptw %>% select(Balances_ptw[[3]]) %>% apply(1, sum) -> Numerator2_ptw ; Data_ptw %>% select(Balances_ptw[[2]]) %>% apply(1, sum) -> Denominator_ptw ; Data_ptw %>% select(Balances_ptw[[4]]) %>% apply(1, sum) -> Denominator2_ptw
+Data_ptw %>% mutate( Balance = log10(Numerator_ptw/Denominator_ptw), Balance2 = log10(Numerator2_ptw/Denominator2_ptw), Cohort = Data_c_ptw$Cohort ) -> Data_b_ptw
+Data_b_ptw %>% ggplot(aes(x=Cohort, y= Balance, col=Cohort))  + geom_violin() + theme_bw() + coord_flip() + geom_jitter() + stat_summary(fun = "median",geom = "crossbar", aes(color = Cohort)) + stat_summary(fun = "mean", geom = "point", color= "black") 
+Data_b_ptw %>% ggplot(aes(x=Cohort, y= Balance2, col=Cohort))  + geom_violin() + theme_bw() + coord_flip() + geom_jitter() + stat_summary(fun = "median",geom = "crossbar", aes(color = Cohort)) + stat_summary(fun = "mean", geom = "point", color= "black")
+
+#Linear model
+Compute_CLR_taxonomy(Data_ptw, Keep_column = c("UNMAPPED", "UNINTEGRATED")) -> Data_ptw2
+Association_analysis( Prevalence_ptw, Data_ptw2, Data_c_ptw, Meta=Read_number ,FILTER=30) -> Association_results_ptw
+
+#Check if association results are reproduced in the Balance analysis 
+Balances_results_ptw = tibble(Bug = unique(c(Balances_ptw[[1]], Balances_ptw[[3]])), Direction_balance = "Positive")
+Balances_results_ptw = rbind(Balances_results_ptw, tibble(Bug = unique(c(Balances_ptw[[2]], Balances_ptw[[4]])), Direction_balance = "Negative") )
+#2/4 bugs in balances are nominally significant
+left_join(Association_results_ptw,Balances_results_ptw) -> Association_results_ptw
+
+#Permutations
+Null_distribution_ptw= c()
+for (Permutation_n in seq(Permutation_number) ){
+  Data_perm = Data_c_ptw
+  Data_perm$Cohort = sample(Data_c_ptw$Cohort, replace = F,size = dim(Data_c_ptw)[1] )
+  Association_analysis(Prevalence_ptw, Data_ptw2, Data_perm, FILTER=30) -> Association_results_p
+  Null_distribution = c(Null_distribution_ptw, Association_results_p$`Pr(>|t|)_norm`)
+}
+
+sapply( Association_results_ptw$`Pr(>|t|)_norm`, function(x){ sum( Null_distribution <= x  )/Permutation_number  } ) -> FDR_perm_ptw
+FDR_perm_ptw[FDR_perm_ptw > 1] = 1
+Association_results_ptw %>% mutate(FDR_permutation = FDR_perm_ptw ) -> Association_results_ptw
+
+
+Association_results_ptw %>% ggplot(aes(x=Estimate_norm, y= -log10(`Pr(>|t|)_norm`), shape= is.na(Direction_balance) )) +
+  geom_hline( yintercept = -log10(0.05), color="red" ) + geom_point() + theme_bw() +
+  geom_label_repel(data = Top_results %>% filter(FDR_permutation<0.05) %>% unique(), 
+                   aes(label = Bug),
+                   color = "black")
+write_tsv(Association_results_ptw, "Summary_statistics_PathwayAnalysis.csv")  
